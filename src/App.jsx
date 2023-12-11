4ae2d14e2a0fc902c437dda0a3324102b1ed362b
Joshua Scott <scottjm@nsuok.edu>
2023-12-11 10:25:28 -0600

    buttons done

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,41 @@ function App() {
   
   const [ state, setState] = useState("")
 
-function getValue(){
-  e => setState(value+e.target.value)
 
-}
 
   
   return (<>
   <form action="">
   <input id="calc-input" type = "text" value = {state}/>
+  <div>
+  <input class ="text-button" type = "text" value = "Clear"/>
+  <input class ="text-button" type = "text" value = "Delete"/>
+
+  </div>
     <div id= "fourth-button-set">
-      <input type="button" value ="7" onClick = {e => setState(state + e.target.value)}/>
-      <input type="button" value ="8" onClick = {e => setState(state + e.target.value)}/>
-      <input type="button" value ="9" onClick = {e => setState(state + e.target.value)}/>
-      <input type="button" value ="/" onClick = {e => setState(state + e.target.value)}/>
+      <input class = "calc-button" type="button" value ="7" onClick = {e => setState(state + e.target.value)}/>
+      <input class = "calc-button" type="button" value ="8" onClick = {e => setState(state + e.target.value)}/>
+      <input class = "calc-button"type="button" value ="9" onClick = {e => setState(state + e.target.value)}/>
+      <input class = "special-calc-button" type="button" value ="/" onClick = {e => setState(state + e.target.value)}/>
     </div>
     <div id= "third-button-set">
-      <input type="button" value ="4" onClick = {e => setState(state + e.target.value)}/>
-      <input type="button" value ="5" onClick = {e => setState(state + e.target.value)}/>
-      <input type="button" value ="6" onClick = {e => setState(state + e.target.value)}/>
-      <input type="button" value ="*" onClick = {e => setState(state + e.target.value)}/>
+      <input class = "calc-button" type="button" value ="4" onClick = {e => setState(state + e.target.value)}/>
+      <input class = "calc-button" type="button" value ="5" onClick = {e => setState(state + e.target.value)}/>
+      <input class = "calc-button" type="button" value ="6" onClick = {e => setState(state + e.target.value)}/>
+      <input class = "special-calc-button" type="button" value ="*" onClick = {e => setState(state + e.target.value)}/>
 
     </div>
     <div id= "second-button-set">
-      <input type="button" value ="1" onClick = {e => setState(state + e.target.value)}/>
-      <input type="button" value ="2" onClick = {e => setState(state + e.target.value)}/>
-      <input type="button" value ="3" onClick = {e => setState(state + e.target.value)}/>
-      <input type="button" value ="-" onClick = {e => setState(state + e.target.value)}/>
+      <input class = "calc-button" type="button" value ="1" onClick = {e => setState(state + e.target.value)}/>
+      <input class = "calc-button" type="button" value ="2" onClick = {e => setState(state + e.target.value)}/>
+      <input class = "calc-button" type="button" value ="3" onClick = {e => setState(state + e.target.value)}/>
+      <input class = "special-calc-button" type="button" value ="-" onClick = {e => setState(state + e.target.value)}/>
     </div>
     <div id= "first-button-set">
-      <input type="button" value ="0" onClick = {e => setState(state + e.target.value)}/>
-      <input type="button" value ="." onClick = {e => setState(state + e.target.value)}/>
-      <input type ="button" value ="=" onClick = {e => setState(eval(state))}/>
-      <input type="button" value ="+" onClick = {e => setState(state + e.target.value)}/>
+      <input class = "calc-button" type="button" value ="0" onClick = {e => setState(state + e.target.value)}/>
+      <input class = "special-calc-button" type="button" value ="." onClick = {e => setState(state + e.target.value)}/>
+      <input class = "special-calc-button" type ="button" value ="=" onClick = {e => setState(eval(state))}/>
+      <input class = "special-calc-button" type="button" value ="+" onClick = {e => setState(state + e.target.value)}/>
     </div>
   </form>
   </>
